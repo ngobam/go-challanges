efe8aae4983bc07a14887f0e84f772f1c076676a
mewt2o <brosfandy@gmail.com>
2026-01-20 04:57:32 +0700

    init

--- 0-stock-aggregator/main.go
+++ 0-stock-aggregator/main.go
@@ -8,23 +8,12 @@ import (
 
 func AggregateStockPrice(symbol string, sources []StockSource, timeout time.Duration) (StockPrice, error) {
 	// TODO: Implement this function
-	//
-	// Hints:
-	// 1. Create a channel to receive stock prices
-	// 2. Launch a goroutine for each source
-	// 3. Use select with time.After for timeout
-	// 4. Return the first result received
-	// 5. Consider using context for cancellation
 
 	panic("not implemented")
 }
 
 func AggregateStockPriceWithContext(ctx context.Context, symbol string, sources []StockSource) (StockPrice, error) {
 	// TODO: Implement this function
-	//
-	// Hints:
-	// 1. Similar to above but use ctx.Done() instead of time.After
-	// 2. This allows external cancellation control
 
 	panic("not implemented")
 }
